60c1cb7a086c0f80698ea94f4bbe37520394a77b
Shiven Shekar <shivenshekar01@gmail.com>
2025-03-01 18:19:36 -0700

    Initial project setup

---
+++ Project Part 1/src/main/java/com/shivenshekar/App.java
@@ -0,0 +1,13 @@
+package com.shivenshekar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
